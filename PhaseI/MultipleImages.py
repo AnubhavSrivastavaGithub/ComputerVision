17a93863fcb82c11379f2da57baa69f2c7f0bb84
ANUBHAV SRIVASTAVA <50038824+AnubhavSrivastavaGithub@users.noreply.github.com>
2020-03-01 00:32:09 +0530

    Update MultipleImages.py

--- PhaseI/MultipleImages.py
+++ PhaseI/MultipleImages.py
@@ -7,3 +7,9 @@ cv2.imshow('Image1',img)
 cv2.imshow('Image2',img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+// Load Two Images simultaneously and show them in two different windows 'Image1' & 'Image2'.
+// Both the two Images city.jpg & watch.jpg are in the same directory.
+// Both the images can be of different sizes.
+// Both the images can be of different color codes.
+// Enter any key to destroy windows.
